1be4a714c014cb615a1c079e398114edb66e6c51
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-16 12:41:40 +0100

    move style sheet too top of src/ for basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import "./App.css";
+
 export const App = () => {
   return (
     <>
-      <h2>Website Under Construction</h2>;
+      <h2>Website Under Construction</h2>
     </>
   );
 };
