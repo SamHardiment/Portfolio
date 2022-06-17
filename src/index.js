ca412696e3b064e899dff2b364ba60381035bf5b
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-17 15:38:23 +0100

    add a Reducer folder for scrollrefreducers, wrap app component in a Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 import { App } from "./App";
 import "./index.css";
+import scrollRefReducer from "./Reducers/scrollRefReducer";
+const store = createStore(scrollRefReducer, devToolsEnhancer());
 
 ReactDom.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
