417458624ca4ad962bda6b0c7f08ec547b41ce3f
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-20 12:11:21 +0100

    add Header component to layout folder

---
+++ src/layout/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+
+export const Header = () => {
+  return (
+    <div class="heroText">
+      <h1 class="heroTitle">A Generic Gretting</h1>
+      <h2 class="heroSub">Brought to you by Samuel Hardiment</h2>
+    </div>
+  );
+};
