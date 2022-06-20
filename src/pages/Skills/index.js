c906667c9bc9a2eac3c040be30697d636a21198e
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-20 14:37:20 +0100

    add more image svg to skill carousel

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -6,6 +6,14 @@ import rt from "../../images/react-seeklogo.com.svg";
 import node from "../../images/node-node-js-seeklogo.com.svg";
 import jt from "../../images/jest-seeklogo.com.svg";
 
+import redux from "../../images/redux-seeklogo.com.svg";
+import docker from "../../images/docker-seeklogo.com.svg";
+import netlify from "../../images/netlify-seeklogo.com.svg";
+import postgresql from "../../images/postgresql-seeklogo.com.svg";
+import heroku from "../../images/heroku-seeklogo.com.svg";
+import mongodb from "../../images/mongodb-seeklogo.com.svg";
+import bootstrap from "../../images/bootstrap-seeklogo.com.svg";
+
 import "./index.css";
 
 export const Skills = () => {
@@ -16,11 +24,19 @@ export const Skills = () => {
         <div className="underline-2"></div>
       </div>
       <div className="carousel-section">
+        {/* TO DO: write a map to create images from an arrary */}
         <Carousel>
           <img src={js} alt="Javascript Logo" />
           <img src={rt} alt="React Logo" />
           <img src={node} alt="NodeJS Logo" />
           <img src={jt} alt="Jest Logo" />
+          <img src={redux} alt="Redux Logo" />
+          <img src={docker} alt="Docker Logo" />
+          <img src={netlify} alt="NetlifyLogo" />
+          <img src={postgresql} alt="PostgreSQL Logo" />
+          <img src={heroku} alt="Heroku Logo" />
+          <img src={mongodb} alt="MongoDB Logo" />
+          <img src={bootstrap} alt="Bootstrap Logo" />
         </Carousel>
       </div>
     </div>
