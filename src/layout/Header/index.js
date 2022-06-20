3cc3ca42a3eefb46c6fd1251e150cb4f8663c537
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-20 12:31:04 +0100

    style header and add welcome text

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -3,9 +3,11 @@ import "./index.css";
 
 export const Header = () => {
   return (
-    <div class="heroText">
-      <h1 class="heroTitle">A Generic Gretting</h1>
-      <h2 class="heroSub">Brought to you by Samuel Hardiment</h2>
+    <div className="header">
+      <div class="hero-text-container">
+        <h1 class="hero-title">A Generic Gretting</h1>
+        <h2 class="hero-subtext">Brought to you by Samuel Hardiment</h2>
+      </div>
     </div>
   );
 };
